bfd6c2624d8719e6aaf184d2fe0bf6d43195a94c
Aman Deep <deep.aman1729@gmail.com>
2023-03-06 17:49:34 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -4,62 +4,6 @@ from django.db import connection
 import json
 from django.db import connection
 
-
-
-
-# Create your views here.
-
-# load_dotenv()
-
-# client_id = os.getenv("CLIENT_ID")
-# client_secret = os.getenv("CLIENT_SECRET")
-
-
-# def getToken():
-    
-#     auth_string = client_id+':'+client_secret
-#     auth_bytes = auth_string.encode('utf-8')
-#     auth_base64 = str(base64.b64encode(auth_bytes),'utf-8')
-
-#     url = "https://accounts.spotify.com/api/token"
-
-#     headers={
-#         "Authorization": 'Basic '+ auth_base64,
-#         "Content-Type":'application/x-www-form-urlencoded'
-#     }
-
-#     data ={
-#          "grant_type": 'client_credentials'
-#     }
-
-
-#     result = post(url , headers=headers , data=data)
-
-#     result_json = json.loads(result.content)
-#     return result_json['access_token']
-
-    
-# @api_view(['GET'])
-# def get_music(request):
-#     sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
-#     data = []
-#     results = sp.playlist_items('0vvXsWCC9xrXsKd4FyS8kM',limit=5, market=None)
-    
-#     for idx, item in enumerate(results['items']):
-#         itemObj = {}
-#         track = item['track']
-#         album = track['album']
-
-#         itemObj['track_id'] = track['id']
-#         itemObj['track_name'] = track['name']
-#         itemObj['track_link'] = track['external_urls']['spotify']
-#         itemObj['track_cover'] = album['images']
-#         itemObj['artist_name'] = track['artists'][0]['name']
-#         data.append(itemObj)
-
-#     return Response({"data":data,"status":1,"errorMessage":""})
-
-
 def dictfetchall(cursor):
     "Return all rows from a cursor as a dict"
     columns = [col[0] for col in cursor.description]
